a234dd149258104ab7a2aa2d75fdcc5af019ff1a
kelvinchinedu1220 <pilopilosimon1990@gmail.com>
2025-12-07 22:27:04 +0100

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,14 +1,12 @@
 #
-# Copyright (C) 2025 The Android Open Source Project
-# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 The TWRP Open Source Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_a23xq.mk
+    $(LOCAL_DIR)/twrp_a23xq.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_a23xq-user \
-    omni_a23xq-userdebug \
-    omni_a23xq-eng
+    twrp_a23xq-eng
