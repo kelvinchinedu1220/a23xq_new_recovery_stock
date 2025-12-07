490591b233c95c4c61fae5ab4c687082263f8806
kelvinchinedu1220 <pilopilosimon1990@gmail.com>
2025-12-07 22:09:21 +0100

    Revise copyright and enhance build settings
    Updated copyright years and added environment variables for build configuration.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,32 @@
 #
-# Copyright (C) 2025 The Android Open Source Project
-# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 The TWRP Open Source Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_a23xq-user
-add_lunch_combo omni_a23xq-userdebug
-add_lunch_combo omni_a23xq-eng
+# For building with minimal manifest
+export ALLOW_MISSING_DEPENDENCIES="true"
+export OF_DISABLE_MIUI_SPECIFIC_FEATURES="1"
+export OF_DONT_PATCH_ENCRYPTED_DEVICE="1"
+export OF_SCREEN_H="2400"
+export OF_STATUS_H="140"
+export OF_STATUS_INDENT_LEFT="48"
+export OF_STATUS_INDENT_RIGHT="48"
+export OF_USE_MAGISKBOOT="1"
+export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES="1"
+export OF_NO_TREBLE_COMPATIBILITY_CHECK="1"
+export OF_SKIP_MULTIUSER_FOLDERS_BACKUP="1"
+export LC_ALL="C"
+export FOX_USE_NANO_EDITOR="1"
+export FOX_DELETE_AROMAFM="1"
+export FOX_USE_TAR_BINARY="1"
+export FOX_USE_SED_BINARY="1"
+export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER="1"
+export FOX_USE_XZ_UTILS="1"
+export FOX_VARIANT="A12"
+export OF_MAINTAINER="nino"
+export FOX_BUILD_TYPE="Stable"
+export FOX_VERSION="R11.1_0"
+export OF_FL_PATH1="/tmp/flashlight" # See /init.recovery.qcom.rc for more information
+export OF_FL_PATH2=""
